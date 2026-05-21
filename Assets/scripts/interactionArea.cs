96eae8c7a42f0bb580ff7c7283d70ce1a6b36b1e
Felidownti <49739856@est.ort.edu.ar>
2026-05-21 12:15:29 -0300

    agarrar

---
+++ Assets/scripts/interactionArea.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class interactionArea : MonoBehaviour
+{
+    public UIManager UIManagerScript;
+    public int score = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScore(0);
+    }
+    void Awake()
+    {
+        UIManagerScript = GameObject.FindObjectOfType<UIManager>();
+    }
+    void OnTriggerEnter(Collider other)
+    {
+       if(other.gameObject.CompareTag("Collectible")){
+        Destroy(other.gameObject);
+        score  ++;
+        UIManagerScript.UpdateScore(score);
+       }
+    } 
+}
